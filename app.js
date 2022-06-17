35de16aa911352510429180378de9cfcaed2390f
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-06-16 23:08:20 -0700

    Comment core functions

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const searchBar = document.querySelector('#search-bar');
 const searchButton = document.querySelector('#search-button');
 const containerDiv = document.querySelector('.results');
 
+/**
+ * getRequestParams combines search query with other default request parameters into an object
+ * @param {string} query - search query
+ * @returns request parameters object
+ */
 function getRequestParams(query) {
     return {
         'api_key': API_KEY,
@@ -18,10 +23,20 @@ function getRequestParams(query) {
     }
 }
 
+/**
+ * createRequestLink makes a full link representing the request with parameters in given `params` object
+ * @param {object} params - request parameters object
+ * @returns string of full link to resource
+ */
 function createRequestLink(params) {
     return API_BASE_URL + `?api_key=${params.api_key}&q=${params.query}&limit=${params.limit}&rating=${params.rating}&offset=${params.offset}`;
 }
 
+/**
+ * getResults asynchronously gets GIFs from the API matching the search query
+ * @param {string} link - link representing request to API
+ * @returns an array of GIF objects
+ */
 async function getResults(link) {
     const response = await fetch(link);
     const results = await response.json();
